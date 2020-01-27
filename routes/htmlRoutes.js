946e92fb545ba15e830a5974edb9eece60e782d2
Stephen Caceres <54186983+stephenc93@users.noreply.github.com>
2020-01-26 19:56:16 -0500

    finalized html and api routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,49 +1,36 @@
-var db = require("../models");
+let db = require("../models");
 //var path = require("path");
 
 module.exports = function(app) {
-  // Load index page
-  //app.get("/", function(req, res) {
-  //    res.sendFile(path.join(__dirname,"../public/login.html"));
-  //});
 
   // ROOT page
-  app.get("/", function(req, res) {
-    res.render("index", {
+  app.get("/", function(request, response) {
+    response.render("index", {
     });
   });
 
-  app.get("/signin", function(req, res) {
-    res.render("login", {
+  app.get("/signup", function(request, response) {
+    response.render("signup", {
     });
   });
 
-  app.get("/signup", function(req, res) {
-    res.render("signup", {
+  app.get("/signin", function(request, response) {
+    response.render("login", {
     });
   });
 
-  app.get("/game", function(req, res) {
-    res.render("game", {
+  app.get("/game", function(request, response) {
+    response.render("game", {
     });
   });
 
-    app.get("/score", function(req, res) {
-      res.render("score", {
-      });
-    });
-
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
-      });
+  app.get("/score", function(request, response) {
+    response.render("score", {
     });
   });
 
   // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
-    res.render("404");
+  app.get("*", function(request, response) {
+    response.render("404");
   });
 };
